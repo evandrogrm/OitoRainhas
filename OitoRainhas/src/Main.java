e058e2ca8faa7a4c1893053711e782e5c9dededc
HD <HD@192.168.25.18>
2017-08-18 18:00:02 -0300

    Trabalho feito até 18-08-17 as 17:59

--- OitoRainhas/src/Main.java
+++ OitoRainhas/src/Main.java
@@ -1,29 +1,39 @@
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
+/**
+ * @author Evandro G.
+ *
+ */
 public class Main {
-	
-	private static int[][] entrada;
-	private static int[] avaliacao;
-	private static int[][] selecionados;
-	
+
 	public static void main(String[] args) {
-		entrada = new int[20][8];
-		avaliacao = new int[20];
-		selecionados = new int[10][8];
-		
+
+		int[][] entrada = new int[20][8];
+		int[] avaliacao = new int[entrada.length];
+		int[][] selecionados = new int[entrada.length / 2][entrada[0].length];
+
 		entrada = inicializaPopulacao(entrada);
 		for (int T = 0; T < 100; T++) {
 			avaliacao = avaliaPopulacao(entrada); // conta conflito entre rainhas
-			selecionados = selecionaPais(avaliacao,entrada); // ordena pela avaliação e descarta metade
+			selecionados = selecionaPais(avaliacao, entrada); // ordena pela avaliação e descarta metade
 			entrada = recombinar(selecionados); // gera filhos com a metade gerada
 			entrada = aplicaMutacao(entrada);
 			avaliacao = avaliaPopulacao(entrada); // conta conflito entre rainhas
-			entrada = selecionaProximaEpoca(avaliacao,entrada); // ordena e nao descarta
+			entrada = selecionaProximaEpoca(avaliacao, entrada); // sera utilizado nos proximos metodos
+			for (int i = 0; i < avaliacao.length; i++) {
+				if(avaliacao[i]==0){
+					System.out.println("Encontrado sequencia sem erros: "+entrada[i].toString());
+				}
+			}
 		}
-		
+
 	}
 
+	/**
+	 * ESTE METODO FOI CRIADO PARA FUTURAS EDICOES
+	 */
 	private static int[][] selecionaProximaEpoca(int[] avaliacao2, int[][] entrada2) {
 		boolean changed = true;
 		int tempAva = 0;
@@ -33,11 +43,11 @@ public class Main {
 			for (int i = 0; i < avaliacao2.length - 1; i++) {
 				if (avaliacao2[i + 1] < avaliacao2[i]) {
 					// swap
-					//swap(avaliacao2[i+1],avaliacao2[i]);
+					// swap(avaliacao2[i+1],avaliacao2[i]);
 					tempAva = avaliacao2[i];
 					avaliacao2[i] = avaliacao2[i + 1];
 					avaliacao2[i + 1] = tempAva;
-					//swap(entrada2[i+1],entrada2[i]);
+					// swap(entrada2[i+1],entrada2[i]);
 					tempEnt = entrada2[i];
 					entrada2[i] = entrada2[i + 1];
 					entrada2[i + 1] = tempEnt;
@@ -50,15 +60,91 @@ public class Main {
 	}
 
 	private static int[][] aplicaMutacao(int[][] entrada2) {
-		// TODO Auto-generated method stub
-		return null;
+		Random rnd = ThreadLocalRandom.current();
+		int indice=0,coluna1=0,coluna2=0;
+		while (coluna1==coluna2) {
+			indice = 0 + rnd.nextInt(entrada2.length);
+			coluna1 = 0 + rnd.nextInt(entrada2[0].length);
+			coluna2 = 0 + rnd.nextInt(entrada2[0].length);
+		}
+		//swap
+		int temp = entrada2[indice][coluna1];
+		entrada2[indice][coluna1] = entrada2[indice][coluna2];
+		entrada2[indice][coluna2] = temp;
+		
+		return entrada2;
 	}
 
 	private static int[][] recombinar(int[][] selecionados2) {
-		int[][] filhos = new int[entrada.length][entrada[0].length];
-		
-		
+		int[][] populacao = new int[selecionados2.length * 2][selecionados2[0].length];
+		int[][] filhos = new int[10][selecionados2[0].length];
+		ArrayList<Integer> listaJaCombinados = new ArrayList<>();
+		// listaJaCombinados.add(selecionados2.clone());
+		Random rnd = ThreadLocalRandom.current();
+		int pontoDeCorte = 1 + rnd.nextInt(selecionados2[0].length - 1);
+		int indiceFilhos = 0;
+		for (int i = 0; i < filhos.length / 2; i++) {
+			int controle = 0;
+			int[] filhoEscolhido = new int[2];
+			while (controle < 2) {
+				int escolhido = 0 + rnd.nextInt(selecionados2.length);
+				if (listaJaCombinados.isEmpty() || (!listaJaCombinados.contains(escolhido))) {
+					filhoEscolhido[controle] = escolhido;
+					listaJaCombinados.add(escolhido);
+					controle++;
+				}
+			}
+			int[][] filhosGerados = cutAndCrossFill(selecionados2[filhoEscolhido[0]], selecionados2[filhoEscolhido[1]],	pontoDeCorte);
+			filhos[indiceFilhos] = filhosGerados[0];
+			filhos[indiceFilhos + 1] = filhosGerados[1];
+			indiceFilhos += 2;
+		}
+		for (int i = 0; i < selecionados2.length; i++) {
+			populacao[i] = selecionados2[i].clone(); // pais
+		}
+		indiceFilhos = 0;
+		for (int i = selecionados2.length; i < populacao.length; i++) {
+			populacao[i] = filhos[indiceFilhos]; // filhos
+			indiceFilhos++;
+		}
+
+		return populacao;
+	}
+
+	private static int[][] cutAndCrossFill(int[] array1, int[] array2, int pontoDeCorte) {
+		int[][] filhos = new int[2][array1.length];
+		int[] array = null;
+		int[] arrayOutro = null;
+		for (int i = 0; i < 2; i++) {
+			int c = pontoDeCorte;
+			if (i == 0) {
+				array = array1.clone();
+				arrayOutro = array2.clone();
+			} else {
+				array = array2.clone();
+				arrayOutro = array1.clone();
+			}
+			for (int j = 0; j < pontoDeCorte; j++) {
+				filhos[i][j] = array[j];
+			}
+			for (int k = 0; k < arrayOutro.length; k++) {
+				if (!contemNumeroNoArray(arrayOutro[k], filhos[i])) {
+					filhos[i][c] = arrayOutro[k];
+					c++;
+				}
+			}
+
+		}
 		return filhos;
+	}
+
+	private static boolean contemNumeroNoArray(int num, int[] array) {
+		for (int j = 0; j < array.length; j++) {
+			if (array[j] == num) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private static int[][] selecionaPais(int[] avaliacao2, int[][] entrada2) {
@@ -71,11 +157,11 @@ public class Main {
 			for (int i = 0; i < avaliacao2.length - 1; i++) {
 				if (avaliacao2[i + 1] < avaliacao2[i]) {
 					// swap
-					//swap(avaliacao2[i+1],avaliacao2[i]);
+					// swap(avaliacao2[i+1],avaliacao2[i]);
 					tempAva = avaliacao2[i];
 					avaliacao2[i] = avaliacao2[i + 1];
 					avaliacao2[i + 1] = tempAva;
-					//swap(entrada2[i+1],entrada2[i]);
+					// swap(entrada2[i+1],entrada2[i]);
 					tempEnt = entrada2[i];
 					entrada2[i] = entrada2[i + 1];
 					entrada2[i + 1] = tempEnt;
@@ -83,7 +169,7 @@ public class Main {
 				}
 			}
 		}
-		for (int i = 0; i < entrada2.length / 2; i++) {
+		for (int i = 0; i < selecionados2.length; i++) {
 			selecionados2[i] = entrada2[i];
 		}
 
@@ -91,48 +177,47 @@ public class Main {
 	}
 
 	private static int[] avaliaPopulacao(int[][] entrada2) {
-		int[] avaliacao2 = new int[avaliacao.length];
-		int[] teste = {1,4,6,8,5,2,7,3};
-		entrada2[0]=teste;
+		int[] avaliacao2 = new int[entrada2.length];
+		int[] teste = { 1, 4, 6, 8, 5, 2, 7, 3 };
+		entrada2[0] = teste;
 		int maior = 0;
 		int menor = 0;
 		int principal = 0;
 		for (int i = 0; i < entrada2.length; i++) {
 			for (int j = 0; j < entrada2[0].length; j++) {
-				if(entrada2[i][j] == j+1){
+				if (entrada2[i][j] == j + 1) {
 					principal++;
-					System.out.println("Diagonal principal");
+//					System.out.println("Diagonal principal");
 				}
-				continua:
-				for (int k = j+1; k < entrada2[0].length; k++) {
-					if(k > entrada2[0].length){
+				continua: for (int k = j + 1; k < entrada2[0].length; k++) {
+					if (k > entrada2[0].length) {
 						continue continua;
 					}
-					if(j-k == 1 || k-j == 1)
-					if(entrada2[i][j]-entrada2[i][k]==1 || entrada2[i][j]-entrada2[i][k]==-1){
-						avaliacao2[i]+=1;
-						System.out.println("entrada2["+i+"]["+j+"] conflitou com entrada["+i+"]["+k+"]");
-					}
-					if(entrada2[i][j]>=entrada2[i][k]){
-						maior = j+1;
-						menor = k+1;
-						if(maior-menor == entrada2[i][j]){
-							avaliacao2[i]+=1;
-							System.out.println("entrada2["+i+"]["+j+"] conflitou com entrada["+i+"]["+k+"]");
+					if (j - k == 1 || k - j == 1)
+						if (entrada2[i][j] - entrada2[i][k] == 1 || entrada2[i][j] - entrada2[i][k] == -1) {
+							avaliacao2[i] += 1;
+//							System.out.println("entrada2[" + i + "][" + j + "] conflitou com entrada[" + i + "][" + k + "]");
 						}
-					}else{
-						maior = k+1;
-						menor = j+1;
-						if(maior-menor == entrada2[i][k]){
-							avaliacao2[i]+=1;
-							System.out.println("entrada2["+i+"]["+j+"] conflitou com entrada["+i+"]["+k+"]");
+					if (entrada2[i][j] >= entrada2[i][k]) {
+						maior = j + 1;
+						menor = k + 1;
+						if (maior - menor == entrada2[i][j]) {
+							avaliacao2[i] += 1;
+//							System.out.println("entrada2[" + i + "][" + j + "] conflitou com entrada[" + i + "][" + k + "]");
+						}
+					} else {
+						maior = k + 1;
+						menor = j + 1;
+						if (maior - menor == entrada2[i][k]) {
+							avaliacao2[i] += 1;
+//							System.out.println("entrada2[" + i + "][" + j + "] conflitou com entrada[" + i + "][" + k + "]");
 						}
 					}
 				}
 			}
-			if(principal>1){
-				avaliacao2[i]+=principal;
-				principal=0;
+			if (principal > 1) {
+				avaliacao2[i] += principal;
+				principal = 0;
 			}
 		}
 		return avaliacao2;
@@ -146,7 +231,7 @@ public class Main {
 				entrada2[i][j] = modelo[j];
 			}
 		}
-		//embaralha duplicados
+		// embaralha duplicados
 		int[][] compara = entrada2.clone();
 		for (int i = 0; i < entrada2.length; i++) {
 			for (int j = 0; j < entrada2.length; j++) {
@@ -158,28 +243,27 @@ public class Main {
 		}
 		for (int i = 0; i < entrada2.length; i++) {
 			for (int j = 0; j < entrada2[0].length; j++) {
-				if(j!=0){
-					System.out.print("|"+entrada2[i][j]);
-				}else{
-					System.out.print("entrada2["+i+"]= "+entrada2[i][j]);
+				if (j != 0) {
+//					System.out.print("|" + entrada2[i][j]);
+				} else {
+//					System.out.print("entrada2[" + i + "]= " + entrada2[i][j]);
 				}
 			}
-			System.out.println();
+//			System.out.println();
 		}
 		return entrada2;
 	}
 
 	private static int[] shuffleArray(int[] modelo) {
 		Random rnd = ThreadLocalRandom.current();
-	    for (int i = modelo.length - 1; i > 0; i--){
-	      int index = rnd.nextInt(i + 1);
-	      //swap
-	      int a = modelo[index];
-	      modelo[index] = modelo[i];
-	      modelo[i] = a;
-	    }
-	    return modelo;
+		for (int i = modelo.length - 1; i > 0; i--) {
+			int index = rnd.nextInt(i + 1);
+			// swap
+			int a = modelo[index];
+			modelo[index] = modelo[i];
+			modelo[i] = a;
+		}
+		return modelo;
 	}
 
-	
 }
