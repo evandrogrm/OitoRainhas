369055265a13f81fec49df92bc87549c25064567
Evandro <Evandro@DESKTOP-4NC142Q.home>
2017-08-20 18:11:02 -0300

    Trabalho funcionando e achada solução. 20/08/17 as 18:10

--- OitoRainhas/src/Main.java
+++ OitoRainhas/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -15,18 +16,29 @@ public class Main {
 		int[][] selecionados = new int[entrada.length / 2][entrada[0].length];
 
 		entrada = inicializaPopulacao(entrada);
-		for (int T = 0; T < 100; T++) {
+		for (int T = 0; T < 1000; T++) {
 			avaliacao = avaliaPopulacao(entrada); // conta conflito entre rainhas
 			selecionados = selecionaPais(avaliacao, entrada); // ordena pela avaliação e descarta metade
 			entrada = recombinar(selecionados); // gera filhos com a metade gerada
 			entrada = aplicaMutacao(entrada);
-			avaliacao = avaliaPopulacao(entrada); // conta conflito entre rainhas
-			entrada = selecionaProximaEpoca(avaliacao, entrada); // sera utilizado nos proximos metodos
-			for (int i = 0; i < avaliacao.length; i++) {
-				if(avaliacao[i]==0){
-					System.out.println("Encontrado sequencia sem erros: "+entrada[i].toString());
+			if(avaliacao[0]==0){
+				System.out.println("Encontrada solução!");
+				for (int i = 0; i < entrada[0].length; i++) {
+					System.out.print(entrada[i]);
 				}
+				System.out.println();
 			}
+//			avaliacao = avaliaPopulacao(entrada); // conta conflito entre rainhas
+//			entrada = selecionaProximaEpoca(avaliacao, entrada); // sera utilizado nos proximos metodos
+			
+			/*for (int i = 0; i < avaliacao.length; i++) {
+				if(avaliacao[i]==0){
+					System.out.print("Encontrado sequencia sem erros: "+entrada[i][0]);
+					for (int j = 1; j < entrada[0].length; j++) {
+						System.out.print(entrada[i][j]);
+					}
+				}
+			}*/
 		}
 
 	}
@@ -77,11 +89,12 @@ public class Main {
 
 	private static int[][] recombinar(int[][] selecionados2) {
 		int[][] populacao = new int[selecionados2.length * 2][selecionados2[0].length];
-		int[][] filhos = new int[10][selecionados2[0].length];
+		int[][] filhos = new int[selecionados2.length][selecionados2[0].length];
 		ArrayList<Integer> listaJaCombinados = new ArrayList<>();
-		// listaJaCombinados.add(selecionados2.clone());
+		ArrayList<int[]> listaSelecionados = new ArrayList<>();
 		Random rnd = ThreadLocalRandom.current();
 		int pontoDeCorte = 1 + rnd.nextInt(selecionados2[0].length - 1);
+		System.out.println("Ponto de corte: "+pontoDeCorte);
 		int indiceFilhos = 0;
 		for (int i = 0; i < filhos.length / 2; i++) {
 			int controle = 0;
@@ -100,11 +113,17 @@ public class Main {
 			indiceFilhos += 2;
 		}
 		for (int i = 0; i < selecionados2.length; i++) {
-			populacao[i] = selecionados2[i].clone(); // pais
+			if(!listaSelecionados.contains(selecionados2[i])){
+				populacao[i] = selecionados2[i].clone(); // pais
+				listaSelecionados.add(selecionados2[i].clone());
+			}
 		}
 		indiceFilhos = 0;
 		for (int i = selecionados2.length; i < populacao.length; i++) {
-			populacao[i] = filhos[indiceFilhos]; // filhos
+			if(!listaSelecionados.contains(filhos[indiceFilhos])){
+				populacao[i] = filhos[indiceFilhos]; // filhos
+				listaSelecionados.add(filhos[indiceFilhos]);
+			}
 			indiceFilhos++;
 		}
 
@@ -176,53 +195,6 @@ public class Main {
 		return selecionados2;
 	}
 
-	private static int[] avaliaPopulacao(int[][] entrada2) {
-		int[] avaliacao2 = new int[entrada2.length];
-		int[] teste = { 1, 4, 6, 8, 5, 2, 7, 3 };
-		entrada2[0] = teste;
-		int maior = 0;
-		int menor = 0;
-		int principal = 0;
-		for (int i = 0; i < entrada2.length; i++) {
-			for (int j = 0; j < entrada2[0].length; j++) {
-				if (entrada2[i][j] == j + 1) {
-					principal++;
-//					System.out.println("Diagonal principal");
-				}
-				continua: for (int k = j + 1; k < entrada2[0].length; k++) {
-					if (k > entrada2[0].length) {
-						continue continua;
-					}
-					if (j - k == 1 || k - j == 1)
-						if (entrada2[i][j] - entrada2[i][k] == 1 || entrada2[i][j] - entrada2[i][k] == -1) {
-							avaliacao2[i] += 1;
-//							System.out.println("entrada2[" + i + "][" + j + "] conflitou com entrada[" + i + "][" + k + "]");
-						}
-					if (entrada2[i][j] >= entrada2[i][k]) {
-						maior = j + 1;
-						menor = k + 1;
-						if (maior - menor == entrada2[i][j]) {
-							avaliacao2[i] += 1;
-//							System.out.println("entrada2[" + i + "][" + j + "] conflitou com entrada[" + i + "][" + k + "]");
-						}
-					} else {
-						maior = k + 1;
-						menor = j + 1;
-						if (maior - menor == entrada2[i][k]) {
-							avaliacao2[i] += 1;
-//							System.out.println("entrada2[" + i + "][" + j + "] conflitou com entrada[" + i + "][" + k + "]");
-						}
-					}
-				}
-			}
-			if (principal > 1) {
-				avaliacao2[i] += principal;
-				principal = 0;
-			}
-		}
-		return avaliacao2;
-	}
-
 	private static int[][] inicializaPopulacao(int[][] entrada2) {
 		int[] modelo = { 1, 2, 3, 4, 5, 6, 7, 8 };
 		for (int i = 0; i < entrada2.length; i++) {
@@ -265,5 +237,45 @@ public class Main {
 		}
 		return modelo;
 	}
+	
+	private static int[] avaliaPopulacao(int[][] entrada2) {
+		int[] avaliacao2 = new int[entrada2.length];
+//		int[] teste = {1,6,3,5,4,7,2,8};
+//		entrada2[0] = teste;
+		int maiorVetor;
+		int menorVetor;
+		int maiorIndice;
+		int menorIndice;
+		for (int v = 0; v < entrada2.length; v++) {//cada entrada
+			for (int i = 0; i < entrada2[0].length; i++) {//cada casa
+				for (int j = 0; j < entrada2[0].length; j++) {//comparacao
+					if(i==j){
+						continue;
+					}
+					if(entrada2[v][i]>=entrada2[v][j]){
+						maiorVetor = entrada2[v][i];
+						menorVetor = entrada2[v][j];
+					}else{
+						maiorVetor = entrada2[v][j];
+						menorVetor = entrada2[v][i];
+					}
+					if(i > j){
+						maiorIndice = i;
+						menorIndice = j;
+					}else{
+						maiorIndice = j;
+						menorIndice = i;
+					}
+					if(maiorVetor - menorVetor == maiorIndice - menorIndice){
+						avaliacao2[v]++;
+					}
+				}
+			}
+			avaliacao2[v]=avaliacao2[v]/2;
+		}
+		
+		return avaliacao2;
+	}
+	
 
 }
